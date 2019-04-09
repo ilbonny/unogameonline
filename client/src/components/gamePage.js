b643a0e9400e130e374d17879d1965b8b11c2d6a
marcobonera <marcobonera@MacBook-Pro-di-Marco.local>
2019-04-09 15:06:56 +0200

    add cards positions

--- client/src/components/gamePage.js
+++ client/src/components/gamePage.js
@@ -54,17 +54,60 @@ export class Game extends Component {
 
   }
 
-  createPlayerCard = (position, cardStyle)=>{
+  createPlayerCard = (position, container, cardStyle, transform)=>{
     const {players} = this.state.game;
     const findPlayer = _.find(players, (player) =>{return player.position === position});
     
     if(findPlayer === undefined) return;
-
+    
     return findPlayer.hand.map((card, index) =>{
-        return  (<div className={cardStyle} key={index} style={this.setTransformCardLeft(index, findPlayer.hand.length)} className={card.value + '_' + card.color}></div>)});
-  }  
+        return  (
+          <div className={container} key={index} >
+             <div className={cardStyle + ' '+ card.value + '_' + card.color } 
+               style={this.setTransformCard(transform, index, findPlayer.hand.length)} >
+             </div>
+          </div>
+          )
+        });
+  } 
+  
+  setTransformCard = (transform, index, count)=>{
+    let result = {};
+    switch (transform) {
+      case 'bottom':
+          result = this.transformCard(index, count, 0);
+          return {
+            transform: `rotate(${result.deg}deg)`,
+            left: result.start + "px",
+            top: result.space + "px"
+          }    
+      case 'right':
+          result = this.transformCard(index, count, 90);
+          return {
+            transform: `rotate(${result.deg * -1}deg)`,
+            left: result.space + "px",
+            top: result.start + "px"
+          }   
+      case 'top':
+          result = this.transformCard(index, count, 0);
+          return {
+            transform: `rotate(${result.deg * -1}deg)`,
+            left: result.start + "px",
+            bottom: result.space + "px"
+          }   
+      case 'left':
+          result = this.transformCard(index, count, 90);
+          return {
+            transform: `rotate(${result.deg}deg)`,
+            right: result.space + "px",
+            top: result.start + "px"
+          }
+      default:
+        break;    
+    }
+  }
 
-  setTransformCardLeft = (index, count) => {
+  setTransformCardLeft = (tranform, index, count) => {
     var result = this.transformCard(index, count, 90);
     return {
       transform: `rotate(${result.deg}"deg")`,
@@ -116,27 +159,19 @@ export class Game extends Component {
             <button type="button" className="btn btn-outline-secondary" onClick={this.challengeDrawFour(false)}>No</button>
         </div>     
         <div id="leftdiv">
-            <div className="cardDivContainerVertical">
-                {this.createPlayerCard(4, 'cardVerticalLeft')}
-            </div>
+            {this.createPlayerCard(4,'cardDivContainerVertical','cardVerticalLeft', 'left')}
         </div>   
         <div id="topdiv">
-            <div className="cardDivContainerHorizontal">
-                {this.createPlayerCard(3, 'cardHorizontal')}
-            </div>
+            {this.createPlayerCard(3, 'cardDivContainerHorizontal','cardHorizontal', 'top')}            
         </div>
         <div id="arrowsdiv">
            
         </div>
         <div id="rightdiv">
-            <div className="cardDivContainerVertical">
-                {this.createPlayerCard(2, 'cardVerticalRight')}
-            </div>
+            {this.createPlayerCard(2, 'cardDivContainerVertical','cardVerticalRight','right')}
         </div>
         <div id="bottomdiv">
-            <div className="cardDivContainerHorizontal">
-                {this.createPlayerCard(1, 'cardHorizontal')}
-            </div>            
+            {this.createPlayerCard(1, 'cardDivContainerHorizontal','cardHorizontal','bottom')}
         </div>
     </div>
     );
