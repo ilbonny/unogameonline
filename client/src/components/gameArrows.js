5a97003344d52b3c6f1eedc4db01d17e149cda9b
marcobonera <marcobonera@MacBook-Pro-di-Marco.local>
2019-04-12 09:05:43 +0200

    add arrows image

--- client/src/components/gameArrows.js
+++ client/src/components/gameArrows.js
@@ -1,26 +1,61 @@
 import React, { Component } from "react";
 import "../resources/main.css";
 
-
 export class GameArrows extends Component {
   setArrowsCurrentColor = () => {
     const { game } = this.props;
     let card = game.discardPile[game.discardPile.length - 1];
-    let arrowurl = game.isReverse
-      ? `../resources/time_reverse_arrows_${card.color}.png`
-      : `../resources/time_arrows_${card.color}.png`;
-
+    let arrowurl = this.selectArrowImage(card.color, game.isReverse);
     const arrowRotation = game.isReverse ? "rightRotation" : "leftRotation";
     return (
       <div id="arrowsdiv">
         <img
-          src={require('../resources/time_arrows_blue.png')}
+          src={arrowurl}
           className={arrowRotation}
           id="arrowsimg"
           alt="arrows"
         />
       </div>
     );
+  };
+
+  selectArrowImage = (color, isReverse) => {
+    const arrowblue = require("../resources/time_arrows_blue.png");
+    const arrowreverseblue = require("../resources/time_reverse_arrows_blue.png");
+    const arrowgreen = require("../resources/time_arrows_green.png");
+    const arrowreversegreen = require("../resources/time_reverse_arrows_green.png");
+    const arrowred = require("../resources/time_arrows_red.png");
+    const arrowreversered = require("../resources/time_reverse_arrows_red.png");
+    const arrowyellow = require("../resources/time_arrows_yellow.png");
+    const arrowreverseyellow = require("../resources/time_reverse_arrows_yellow.png");
+
+    if (isReverse) {
+      switch (color) {
+        case "Red":
+          return arrowreversered;
+        case "Blue":
+          return arrowreverseblue;
+        case "Yellow":
+          return arrowreverseyellow;
+        case "Green":
+          return arrowreversegreen;
+        default:
+          break;
+      }
+    } else {
+      switch (color) {
+        case "Red":
+          return arrowred;
+        case "Blue":
+          return arrowblue;
+        case "Yellow":
+          return arrowyellow;
+        case "Green":
+          return arrowgreen;
+        default:
+          break;
+      }
+    }
   };
 
   render() {
